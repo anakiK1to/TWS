53ccbafa3e71d74ce9d43cfd0a11b8bec5f6ad87
k1to <loksli322@yandex.ru>
2025-03-31 22:45:50 +0300

    Аутентификация в SOAP на CREATE, DELETE, UPDATE

--- standalone_server/src/main/java/ru/itmo/standalone_server/server/WebService.java
+++ standalone_server/src/main/java/ru/itmo/standalone_server/server/WebService.java
@@ -7,19 +7,63 @@ import ru.itmo.standalone_server.model.entity.Person;
 import ru.itmo.standalone_server.service.PersonService;
 import ru.itmo.standalone_server.service.PersonValidation;
 
+import javax.annotation.Resource;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
+import javax.xml.ws.WebServiceContext;
+import javax.xml.ws.handler.MessageContext;
+import java.util.Base64;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 @javax.jws.WebService(    serviceName = "PersonService",
-        targetNamespace = "http://ru.itmo.standalone_server/", // Ваш кастомный namespace
+        targetNamespace = "http://ru.itmo.standalone_server/",
         portName = "PersonServicePort")
 public class WebService {
     private final PersonService personService;
 
+
+    @Resource
+    private WebServiceContext context;
+
     public WebService(PersonService personService) {
         this.personService = personService;
+    }
+
+    private static final String AUTH_USERNAME = "admin";
+    private static final String AUTH_PASSWORD = "password";
+
+    private boolean isAuthenticated() {
+        try {
+            MessageContext messageContext = context.getMessageContext();
+            Map<String, List<String>> headers = (Map<String, List<String>>) messageContext.get(MessageContext.HTTP_REQUEST_HEADERS);
+
+            List<String> authHeaders = headers.get("Authorization");
+            if (authHeaders == null || authHeaders.isEmpty()) {
+                return false;
+            }
+
+            String authHeader = authHeaders.get(0);
+            if (!authHeader.startsWith("Basic ")) {
+                return false;
+            }
+
+            String encodedCredentials = authHeader.substring("Basic ".length());
+            String credentials = new String(Base64.getDecoder().decode(encodedCredentials));
+            String[] parts = credentials.split(":");
+
+            if (parts.length != 2) {
+                return false;
+            }
+
+            String username = parts[0];
+            String password = parts[1];
+
+            return AUTH_USERNAME.equals(username) && AUTH_PASSWORD.equals(password);
+        } catch (Exception e) {
+            return false;
+        }
     }
 
     @WebMethod
